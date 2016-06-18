6c3b65bb101f926a27b0e2d749a353cf73f7969c
Heber Ortiz Pereira <heber.pereira@lambda3.com.br>
2016-06-18 13:44:21 -0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Session;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace RedisApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            HttpContext.Session.SetString("Chave", $"Data: {DateTime.Now}");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            var valor = HttpContext.Session.GetString("Chave");
+
+            ViewData["Message"] = $"Your application description page. {valor}";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
